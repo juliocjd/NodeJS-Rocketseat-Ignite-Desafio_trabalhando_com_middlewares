0e59ff0d16d653aed068272b71b6fefe5f3be23d
juliocjd <julio.justini@gmail.com>
2022-07-08 11:49:40 -0300

    Finished project

--- src/index.js
+++ src/index.js
@@ -10,19 +10,57 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    response.status(404).json({ error: 'User not found' });
+  } else {
+    request.user = user;
+    next()
+  }
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if (user.pro || user.todos.length < 10) {
+    next();
+  } else {
+    response.status(403).json({ error: 'Error' });
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (validate(id)) {
+    const user = users.find(user => user.username === username);
+    if (user) {
+      const todo = user.todos.find(todo => todo.id === id);
+      if (todo) {
+        request.todo = todo;
+        request.user = user;
+        next()
+      } else {
+        response.status(404).json({ error: 'Error' });;
+      }
+    } else {
+      response.status(404).json({ error: 'Error' });;
+    }
+  } else {
+    response.status(400).json({ error: 'Error' });;
+  }
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id)
+  if (user) {
+    request.user = user;
+    next();
+  } else {
+    response.status(404).json({ error: 'Error' });;
+  }
 }
 
 app.post('/users', (request, response) => {
